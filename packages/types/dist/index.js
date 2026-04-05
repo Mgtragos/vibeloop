2a457b6942eda24975b42230102cf0cb38f27dfc
Mac <mac@Macs-MacBook-Pro.local>
2026-04-05 13:23:59 +0300

    chore: clean repo - source files only

--- packages/types/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// ── User ──────────────────────────────────────────────────────────────────────
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=index.js.map
